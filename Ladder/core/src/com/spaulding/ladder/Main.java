1428bc2680ab50b9ddca6e199a2bf83621400185
jared <jaredspaulding1998@gmail.com>
2016-05-18 17:54:26 -0600

    Cleaning

--- Ladder/core/src/com/spaulding/ladder/Main.java
+++ Ladder/core/src/com/spaulding/ladder/Main.java
@@ -3,7 +3,7 @@ package com.spaulding.ladder;
 import com.badlogic.gdx.Game;
 
 public class Main extends Game {
-	
+
 	@Override
 	public void create() {
 
--- Ladder/ios/src/com/spaulding/ladder/IOSLauncher.java
+++ Ladder/ios/src/com/spaulding/ladder/IOSLauncher.java
@@ -5,7 +5,6 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.spaulding.ladder.Main;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
