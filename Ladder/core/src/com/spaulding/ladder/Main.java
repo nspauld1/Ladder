1069dd9c5c33622733af57a7c0805e932c2ef903
jared <jaredspaulding1998@gmail.com>
2016-05-18 17:48:42 -0600

    fixed everything
    fixed everything

--- Ladder/core/src/com/spaulding/ladder/Main.java
+++ Ladder/core/src/com/spaulding/ladder/Main.java
@@ -1,56 +1,11 @@
 package com.spaulding.ladder;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.spaulding.ladder.Entity.Ladder;
-import com.spaulding.ladder.Entity.Object;
-import com.spaulding.ladder.Screen.MenuScreen;
-import com.spaulding.ladder.Screen.ScreenManager;
+import com.badlogic.gdx.Game;
 
-public class Main implements ApplicationListener {
-	public Stage stage;
-	public static int WIDTH = 1080, HEIGHT = 640;
-
+public class Main extends Game {
+	
 	@Override
-	public void create () {
-		stage = new Stage();
-		Object ladder1 = new Ladder(Ladder.LadderType.NORMAL);
-		stage.addActor(ladder1);
-		ScreenManager.setScreen(new MenuScreen());
-	}
+	public void create() {
 
-	@Override
-	public void render () {
-		if (ScreenManager.getCurrentScreen() != null){
-			ScreenManager.getCurrentScreen().render(stage);
-		}
-	}
-
-	@Override
-	public void resize(int width, int height) {
-		if (ScreenManager.getCurrentScreen() != null){
-			ScreenManager.getCurrentScreen().resize(width, height);
-		}
-	}
-
-	@Override
-	public void pause() {
-		if (ScreenManager.getCurrentScreen() != null){
-			ScreenManager.getCurrentScreen().pause();
-		}
-	}
-
-	@Override
-	public void resume() {
-		if (ScreenManager.getCurrentScreen() != null){
-			ScreenManager.getCurrentScreen().resume();
-		}
-	}
-
-	@Override
-	public void dispose() {
-		if (ScreenManager.getCurrentScreen() != null){
-			ScreenManager.getCurrentScreen().dispose();
-		}
 	}
 }
