f1e8b28b494bd45f739463c65e25d598f5c55e19
jared <jaredspaulding1998@gmail.com>
2016-05-19 17:58:44 -0600

    Ladders
    Made ladders with texture region
    worked in assets

--- Ladder/core/src/com/spaulding/ladder/Assets.java
+++ Ladder/core/src/com/spaulding/ladder/Assets.java
@@ -1,13 +1,16 @@
 package com.spaulding.ladder;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
  * Created by jared on 5/18/2016.
  */
 public class Assets {
     public static Texture play, settings, high_score, about, back, title;
-    public static Texture ladder;
+    public static TextureRegion[] ladders = new TextureRegion[6];
+    public static TextureAtlas ladder_atlas;
 
     public static void load(){
         play = new Texture("play.png");
@@ -16,7 +19,13 @@ public class Assets {
         about = new Texture("about.png");
         title = new Texture("title.png");
         back = new Texture("back.png");
+        ladder_atlas = new TextureAtlas("ladder_sheet.atlas");
 
-        ladder = new Texture("ladder.png");
+        ladders[0] = new TextureRegion(ladder_atlas.findRegion("ladder_small_small"));
+        ladders[1] = new TextureRegion(ladder_atlas.findRegion("ladder_small"));
+        ladders[2] = new TextureRegion(ladder_atlas.findRegion("ladder_mediam_mediam"));
+        ladders[3] = new TextureRegion(ladder_atlas.findRegion("ladder_mediam"));
+        ladders[4] = new TextureRegion(ladder_atlas.findRegion("ladder_large_large"));
+        ladders[5] = new TextureRegion(ladder_atlas.findRegion("ladder_large"));
     }
 }
