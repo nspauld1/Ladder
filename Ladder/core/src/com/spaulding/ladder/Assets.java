0e93188d8592917407a3a5b2203598cb6007ddfe
jared <jaredspaulding1998@gmail.com>
2016-05-19 15:27:20 -0600

    Deleted nonesense

--- Ladder/core/src/com/spaulding/ladder/Assets.java
+++ Ladder/core/src/com/spaulding/ladder/Assets.java
@@ -6,11 +6,7 @@ import com.badlogic.gdx.graphics.Texture;
  * Created by jared on 5/18/2016.
  */
 public class Assets {
-<<<<<<< HEAD
     public static Texture play, settings, high_score, about, back, title;
-=======
-    public static Texture play, settings, high_score, about, title, back;
->>>>>>> origin/master
 
     public static void load(){
         play = new Texture("play.png");
